24c43a766378602789e75ae392ac28ba618af344
emwno <pulse.aashir@gmail.com>
2021-02-06 02:16:33 +0500

    create a basic code structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { registerRootComponent } from "expo";
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>Hello</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +21,5 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 	},
 });
+
+export default registerRootComponent(App);
