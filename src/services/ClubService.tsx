904d4042c26b3e7040f7c94f00e7861fc7f8ba55
emwno <pulse.aashir@gmail.com>
2021-02-12 01:53:57 +0500

    create club service to allow api calls for search

---
+++ src/services/ClubService.tsx
@@ -0,0 +1,33 @@
+import { AxiosResponse } from "axios";
+import API from "./API";
+import { Club } from "../types";
+
+export default class ClubService {
+	/**
+	 * Search for clubs based on given query and optional filter (tags)
+	 */
+	static async search(searchQuery: string, tagFilter?: string[]) {
+		const response: AxiosResponse<Club[]> = await API.get<Club[]>(
+			"/clubs/search",
+			{
+				params: {
+					query: searchQuery,
+					filter: tagFilter,
+				},
+			}
+		);
+
+		return response.data;
+	}
+
+	/**
+	 * Get all club tags (sorted alphabetically)
+	 */
+	static async getAllTags() {
+		const response: AxiosResponse<string[]> = await API.get<string[]>(
+			"/clubs/tags"
+		);
+
+		return response.data;
+	}
+}
---
+++ src/types/index.tsx
@@ -0,0 +1,12 @@
+/**
+ * Contains all types for data handling
+ */
+
+interface Club {
+	objectId: string;
+	name: string;
+	logo: string;
+	description: string;
+}
+
+export { Club };
