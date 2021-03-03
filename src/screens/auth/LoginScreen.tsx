72dce2e573999f6da32a3cc6ee631c29725fbb24
sriniv58 <sriniv58@purdue.edu>
2021-03-03 03:21:41 -0500

    setup auth navigation between screens

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -4,8 +4,10 @@ import { Button, Input, Layout } from "@ui-kitten/components";
 import { MaterialIcons } from "@expo/vector-icons";
 import { ContainerStyles } from "../../styles/CommonStyles";
 import UserService from "../../services/UserService";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
+	const navigation = useNavigation();
 	const [email, setEmail] = React.useState("");
 	const [password, setPassword] = React.useState("");
 	const [secureText, setSecureText] = React.useState(true);
@@ -26,31 +28,59 @@ const LoginScreen = () => {
 	);
 
 	return (
-		<Layout style={ContainerStyles.center}>
-			<Input
-				placeholder="Email"
-				label="Email"
-				value={email}
-				style={styles.input}
-				onChangeText={(nameUpdate) => setEmail(nameUpdate)}
-			/>
-			<Input
-				placeholder="Password"
-				label="Password"
-				value={password}
-				style={styles.input}
-				onChangeText={(passUpdate) => setPassword(passUpdate)}
-				accessoryRight={visibleIcon}
-				secureTextEntry={secureText}
-			/>
-			<Button style={styles.input} onPress={() => login()}>
-				Login
-			</Button>
+		<Layout style={ContainerStyles.flexContainer}>
+			<Layout style={ContainerStyles.center}>
+				<Input
+					placeholder="Email"
+					label="Email"
+					value={email}
+					style={styles.input}
+					onChangeText={(nameUpdate) => setEmail(nameUpdate)}
+				/>
+				<Input
+					placeholder="Password"
+					label="Password"
+					value={password}
+					style={styles.input}
+					onChangeText={(passUpdate) => setPassword(passUpdate)}
+					accessoryRight={visibleIcon}
+					secureTextEntry={secureText}
+				/>
+				<Button style={styles.input} onPress={() => login()}>
+					Login
+				</Button>
+			</Layout>
+
+			<Layout style={styles.buttonRow}>
+				<Button
+					appearance="ghost"
+					style={styles.button}
+					onPress={() => navigation.navigate("Forgot Password")}
+				>
+					Forgot Password
+				</Button>
+				<Button
+					appearance="ghost"
+					style={styles.button}
+					onPress={() => navigation.navigate("Signup")}
+				>
+					Signup
+				</Button>
+			</Layout>
 		</Layout>
 	);
 };
 
 const styles = StyleSheet.create({
+	button: {
+		marginStart: 10,
+		marginEnd: 10,
+	},
+	buttonRow: {
+		alignContent: "center",
+		justifyContent: "center",
+		flexDirection: "row",
+	},
 	input: {
 		width: 300,
 		marginBottom: 15,
