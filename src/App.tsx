58058e111f01a5ddbe4e2caa28a9ffc152546831
emwno <pulse.aashir@gmail.com>
2021-03-04 06:11:30 -0500

    add auto input validation support

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { registerRootComponent } from "expo";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "react-native";
 import * as eva from "@eva-design/eva";
-import { ApplicationProvider } from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import RootNavigator from "./navigation/RootNavigator";
 
 function App() {
 	return (
 		<NavigationContainer>
 			{StatusBar.setBarStyle("dark-content", true)}
+			<IconRegistry icons={EvaIconsPack} />
 			<ApplicationProvider {...eva} theme={eva.light}>
 				<RootNavigator />
 			</ApplicationProvider>
