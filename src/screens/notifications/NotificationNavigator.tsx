7710e55fcc0f90c49f945fc4bf6b7b7ff908df0f
Aashir Aumir <pulse.aashir@gmail.com>
2021-03-16 00:10:22 -0400

    add base notification navigator: idea is to create completely new screens when responding to a notification click

---
+++ src/screens/notifications/NotificationNavigator.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const NotificationNavigator = () => (
+	<Stack.Navigator>
+
+	</Stack.Navigator>
+);
+
+export default NotificationNavigator;
