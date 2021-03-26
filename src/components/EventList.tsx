610d56752f2d2066c26bc110484ece4d97766905
Sam Lin <lin854@purdue.edu>
2021-03-26 04:43:16 -0400

    Added styling based on event times to event list

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ScrollView, TouchableWithoutFeedback, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { ContainerStyles } from "../styles/CommonStyles";
 import { Text, List, ListItem, Layout } from "@ui-kitten/components";
 import { Event } from "../types";
@@ -10,6 +10,7 @@ type Props = {
 };
 
 const EventList = (props: Props) => {
+	const curDate = new Date();
 	const navigation = useNavigation();
 
 	const getReadableDate = (d: Date) => {
@@ -30,33 +31,75 @@ const EventList = (props: Props) => {
 			)
 		);
 	};
+	const isPast = (d: Date) => {
+		if (typeof d === "string") {
+			d = new Date(d);
+		}
+		return d < curDate;
+	};
+
+	const isCurrent = (start: Date, end: Date) => {
+		if (typeof start === "string") {
+			start = new Date(start);
+		}
+		if (typeof end === "string") {
+			end = new Date(end);
+		}
+
+		return curDate > start && curDate < end;
+	};
+
 	return (
 		<List
 			data={props.events}
-			renderItem={({ item }) => (
-				<ListItem
-					style={{ margin: 10, flexDirection: "column" }}
-					onPress={() =>
-						navigation.navigate("Event", {
-							title: item.name,
-							screen: "Club",
-							params: {
-								id: item.id,
+			renderItem={({ item }) => {
+				var past;
+				if (isCurrent(item.startTime, item.endTime)) past = 0;
+				else isPast(item.endTime) ? (past = 2) : (past = 1);
+				return (
+					<ListItem
+						style={[
+							styles.default,
+							past < 2 ? styles.upcoming : null,
+							past === 0 ? styles.current : null,
+						]}
+						onPress={() =>
+							navigation.navigate("Event", {
 								title: item.name,
-								role: item.role,
-							},
-						})
-					}
-				>
-					<Text>{item.name}</Text>
-					<Text appearance="hint">
-						{getReadableDate(item.startTime)} to{" "}
-						{getReadableDate(item.endTime)}
-					</Text>
-					<Text appearance="hint">{item.shortLocation}</Text>
-				</ListItem>
-			)}
+								params: {
+									id: item.id,
+									title: item.name,
+									role: item.role,
+								},
+							})
+						}
+					>
+						<Text category="h6">{item.name}</Text>
+						<Text appearance="hint">
+							{getReadableDate(item.startTime)} to{" "}
+							{getReadableDate(item.endTime)}
+						</Text>
+						<Text appearance="hint">{item.shortLocation}</Text>
+					</ListItem>
+				);
+			}}
 		/>
 	);
 };
+
+/*	For lack of a better option, add styling based on event timing
+	If an event has passed, do not add border
+	If an event has not ended, add black border
+	If an event is currently ongoing, change border to red
+ */
+const styles = StyleSheet.create({
+	default: {
+		margin: 10,
+		flexDirection: "column",
+		alignItems: "flex-start",
+	},
+	upcoming: { borderWidth: 1, borderRadius: 5 },
+	current: { borderColor: "red" },
+});
+
 export default EventList;
