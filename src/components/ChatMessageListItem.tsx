b0c8823a7c7c17b47e0c79f19bfed88321e7bc1b
sriniv58 <sriniv58@purdue.edu>
2021-04-01 03:19:30 -0400

    Updated to better looking message UI

--- src/components/ChatMessageListItem.tsx
+++ src/components/ChatMessageListItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-import { Avatar, ListItem, Text } from "@ui-kitten/components";
+import { StyleSheet, View } from "react-native";
+import { Avatar, Text } from "@ui-kitten/components";
 import { ChatMessage } from "../types";
 
 interface Props {
@@ -10,26 +10,55 @@ interface Props {
 const ChatMessageListItem = (props: Props) => {
 	const { message } = props;
 	return (
-		<ListItem
-			title={message.authorName}
-			description={message.body}
-			accessoryLeft={() => (
+		<View style={styles.messageContainer}>
+			<View style={styles.avatarContainer}>
 				<Avatar
 					source={{ uri: message.authorPicture }}
-					style={{ marginRight: 5, height: 25, width: 25 }}
+					style={styles.avatar}
 				/>
-			)}
-			accessoryRight={() => (
-				<Text appearance="hint" numberOfLines={1}>
-					{new Date(message.timestamp).getTime()}
+			</View>
+			<View style={styles.textContainer}>
+				<Text style={styles.name} category="s2" appearance="hint">
+					{message.authorName}
 				</Text>
-			)}
-		/>
+				<Text style={styles.body}>{message.body}</Text>
+			</View>
+			<View style={styles.space} />
+		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-
+	messageContainer: {
+		flexDirection: "row",
+		marginTop: 16,
+	},
+	avatarContainer: {
+		flex: 1,
+		justifyContent: "flex-end",
+		marginRight: 8,
+	},
+	avatar: {
+		height: 25,
+		width: 25,
+	},
+	textContainer: {
+		flex: 10,
+		alignItems: "flex-start",
+	},
+	name: {
+		paddingHorizontal: 8,
+	},
+	body: {
+		color: "white",
+		backgroundColor: "green",
+		borderRadius: 15,
+		paddingHorizontal: 8,
+		paddingVertical: 8,
+	},
+	space: {
+		flex: 2,
+	},
 });
 
 export default ChatMessageListItem;
