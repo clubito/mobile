adacb49a1b0cc5aaa3457ed77055037c00f2e0c9
Arjun <sriniv58@purdue.edu>
2021-04-02 15:29:53 -0400

    fixed announcement display

--- src/screens/clubs/AnnouncementList.tsx
+++ src/screens/clubs/AnnouncementList.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { View } from "react-native";
+import { RefreshControl, StyleSheet, View } from "react-native";
 import { ContainerStyles } from "../../styles/CommonStyles";
-import { Text, List, ListItem } from "@ui-kitten/components";
+import { Text, List, ListItem, Divider } from "@ui-kitten/components";
 import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { Announcement } from "../../types";
 import { ClubTabsParamList } from "./ClubScreen";
+import { getReadableDate } from "../../utils";
 
 type AnnouncementListRouteProp = RouteProp<ClubTabsParamList, "AnnouncementList">;
 
@@ -20,21 +21,60 @@ type Props = {
 };
 
 const AnnouncementList = (props: Props) => {
-	const announcements: Announcement[] = props.route.params.announcementList;
-
 	return (
 		<View style={ContainerStyles.horizMargin}>
 			<List
-				data={announcements}
-				renderItem={({ item }) => (
-					<View style={ContainerStyles.extraMargin}>
-						<Text>{item.message}</Text>
-						<Text appearance="hint">{item.timestamp}</Text>
-					</View>
-				)}
+				style={styles.list}
+				data={props.route.params.announcementList}
+				ItemSeparatorComponent={Divider}
+				renderItem={({ item }) => {
+					return (
+						<ListItem
+							style={styles.container}
+							description={() => (
+								<View>
+									<Text style={styles.row}>
+										{item.message}
+									</Text>	
+									<Text style = {styles.row} appearance="hint" numberOfLines={1}>
+										{getReadableDate(item.timestamp)}
+									</Text>
+								</View>
+							)}
+						/>
+					)
+				}}
 			/>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: { 
+		margin: 5,
+	},
+	list: {
+		backgroundColor: "white",
+		marginLeft: -10,
+		marginRight: -10
+	},
+	icon: {
+		flex: 1,
+		marginRight: 5,
+		height: 12,
+		width: 12,
+	},
+	avatar: {
+		height: 50,
+		width: 50,
+	},
+	row: {
+		flexDirection: "row",
+	},
+	infoContainer: {
+		marginLeft: 8,
+	},
+});
+
 
 export default AnnouncementList;
